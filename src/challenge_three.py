5d505d4b7c9f541c86fa23803832e8cddee5cf78
stefanocascavilla <cascavilla1@gmail.com>
2021-06-16 09:19:42 +0200

    Fixed the dict key to be more clear

--- src/challenge_three.py
+++ src/challenge_three.py
@@ -2,14 +2,14 @@ def list_uniqueness(the_list):
     '''
     Return a dictionary with two key-value pairs:
       1. The key 'list_length' stores the length of the_list as its value.
-      2. The key 'unique_items' stores the number of unique items in the_list as its value.
+      2. The key 'number_of_unique_items' stores the number of unique items in the_list as its value.
     Arguments
     the_list: A list
     Examples
     l = [1, 2, 2, 4]
     dictionary = list_uniqueness(l)
     print(dictionary['list_length']) # prints 4
-    print(dictionary['unique_items']) # prints 3
+    print(dictionary['number_of_unique_items']) # prints 3
     '''
 
     # ====================================
@@ -28,4 +28,4 @@ if __name__ == '__main__':
     l = [1, 2, 2, 4]
     dictionary = list_uniqueness(l)
     print(dictionary['list_length'])
-    print(dictionary['unique_items'])
+    print(dictionary['number_of_unique_items'])
